64c6cbaf8703efea1b956a2545f46d8847616ae4
Pandurang Gangaji <103135709+uddheshwar@users.noreply.github.com>
2024-06-20 08:09:06 +0000

    Container Committe

---
+++ basics/test.js
@@ -0,0 +1,12 @@
+let a=("Uddheshwar Maharaj Ki Jay");
+var b= ("Sawargaon");
+var c= ("TQ: Deoni, Dist:- Latur");
+p="names";
+
+//  node basics/test.js
+// prefer let keyword instead of var because of issue in blck scope and functional scope
+
+
+
+console.log(a)
+console.table([a,b,c])
