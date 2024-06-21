af8ebe76c089410c0c4a5dc0bfb6174875300678
Pandurang Gangaji <103135709+uddheshwar@users.noreply.github.com>
2024-06-21 10:54:30 +0000

    data_types

--- basics/data_types.js
+++ basics/data_types.js
@@ -11,7 +11,7 @@ let pincode= (413519)
 let type =(true)
 let arraylist=["Pandurang" ,"Ganpat"]
 let objectname={TQ:"Deoni", DIST:"Latur"}
-
+const bignumber= 12336454645895446569n
 
 const myfunction= function(){
     console.log("Hello World")
@@ -23,5 +23,6 @@ let a= typeof(name)
 let arraytype=typeof(arraylist)
 let objectype=typeof(objectname)
 let fuctiontype=typeof(myfunction)
-console.log(name, pincode, booleantype, nametype,a,arraylist,arraytype,objectname,objectype,myfunction,fuctiontype)
+let bignumbertype=typeof(bignumber)
+console.log(name, pincode, booleantype, nametype,a,arraylist,arraytype,objectname,objectype,myfunction,fuctiontype,bignumber,bignumbertype)
 
