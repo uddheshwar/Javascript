3bbff96052bfea2e93430fc60018d0018b02556f
Pandurang Gangaji <103135709+uddheshwar@users.noreply.github.com>
2024-06-21 08:23:48 +0000

    data types

---
+++ basics/data_types.js
@@ -0,0 +1,18 @@
+// Primitive data types
+// string, number, boolean, null, undefined,symbol
+
+
+// Reference/Non-primitive data types
+// array, object, function
+
+
+let name=("Gangaji Pandurang Bharat")
+let pincode= (413519);
+let type =(true)
+let booleantype=typeof(type)
+let nametype= typeof(name)
+let a= typeof(string)
+
+
+console.log(name, pincode, booleantype, nametype,a)
+
