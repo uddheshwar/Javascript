88b919630d1118febda44e6207960f671cf08fb7
Pandurang Gangaji <103135709+uddheshwar@users.noreply.github.com>
2024-06-21 10:46:30 +0000

    data_types

--- basics/data_types.js
+++ basics/data_types.js
@@ -7,12 +7,21 @@
 
 
 let name=("Gangaji Pandurang Bharat")
-let pincode= (413519);
+let pincode= (413519)
 let type =(true)
+let arraylist=["Pandurang" ,"Ganpat"]
+let objectname={TQ:"Deoni", DIST:"Latur"}
+
+
+const myfunction= function(){
+    console.log("Hello World")
+}
+
 let booleantype=typeof(type)
 let nametype= typeof(name)
-let a= typeof(string)
-
-
-console.log(name, pincode, booleantype, nametype,a)
+let a= typeof(name) 
+let arraytype=typeof(arraylist)
+let objectype=typeof(objectname)
+let fuctiontype=typeof(myfunction)
+console.log(name, pincode, booleantype, nametype,a,arraylist,arraytype,objectname,objectype,myfunction,fuctiontype)
 
