5d8585f4183e58489d9a9111ab4ff29882debc13
Pandurang Gangaji <103135709+uddheshwar@users.noreply.github.com>
2024-06-22 08:11:23 +0000

    string

---
+++ basics/string.js
@@ -0,0 +1,21 @@
+let name = ("Pandurang")
+let fathername = ("Bharat")
+let surname = ("Gangaji")
+
+console.log(name,fathername,surname)
+
+
+//   --------- a) anchor() ---------
+
+//  Note: All HTML wrapper methods are deprecated and only standardized for compatibility purposes. 
+// Use DOM APIs such as document.createElement() instead.
+// The HTML specification no longer allows the <a> element to have a name attribute, 
+// so this method doesn't even create valid markup.
+
+
+var newelement = document.createElement('span');
+
+
+console.log(tag);
+
+
