127786a166e7d4cd0f4051b424eaf3ffb14c074d
Pandurang Gangaji <103135709+uddheshwar@users.noreply.github.com>
2024-06-25 18:26:12 +0000

    function

---
+++ basics/function.js
@@ -0,0 +1,74 @@
+//  ++++++++++++     function     +++++++++++++   //
+//  function is used to execute the many javascript activities 
+   
+           //  +++++  1st method  +++++  //
+
+     function address () {
+        console.log("Name: Pandurang Bharat Gangaji");
+        console.log("Tq: Deoni, Dist:- Latur ");
+        return address()
+     }   
+
+     address();
+
+
+
+
+
+           //  +++++  2nd method  +++++  //
+// example of depending upon money how many bottles of milk can buy
+
+/* function getmoney(money){
+console.log("you  can buy bottles :"+ Math.floor(money/1.4));
+return getmoney
+}
+getmoney(5);*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
