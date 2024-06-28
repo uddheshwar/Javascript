2c14ffb4ab74b6ea4b441d5307992712160e0185
Pandurang Gangaji <103135709+uddheshwar@users.noreply.github.com>
2024-06-28 12:16:53 +0000

    array.js

---
+++ basics/array.js
@@ -0,0 +1,11 @@
+  
+
+
+  names=["Pandurang","ganpat","abhay","avinash"];
+
+  prompt("what is your name ?")
+
+  if(names=== "Pandurang" ||"ganpat" ||"abhay" ||"avinash"){
+     alert("Welcome");}
+
+  else{ alert("please sign in");}
