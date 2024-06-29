178f531eb4048601ffac2072fbdf8b2f1b784006
Pandurang Gangaji <103135709+uddheshwar@users.noreply.github.com>
2024-06-29 08:50:20 +0000

    if_else

--- basics/if_else.js
+++ basics/if_else.js
@@ -25,4 +25,30 @@
    
     
        
-    
+    // ------- increasing count by 1, addng "fizz" at divisable by 3 and
+    // adding "Buzz" at divisable by 5 and adding "fizBuzz" at divisable by 
+    //  3 and 5
+
+
+    var output = [];
+var count = 1;
+
+function fizBuzz() {
+ output.push(count);
+ count++;
+    console.log(output);
+ 
+if(count%5===0 && count%3===0){
+  output.push("fizBuzz")
+}
+ 
+ else if (count%3===0){
+  output.push("fizz")
+ }
+
+ else if(count%5===0){
+  output.push("Buzz")
+ }
+
+}
+
